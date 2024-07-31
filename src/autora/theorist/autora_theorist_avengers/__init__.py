322050618f4bf2a644963a9a6eaf96835f140a8a
Alexander Gerhard Ditz <159267896+AlexanderDitz@users.noreply.github.com>
2024-07-31 08:47:51 +0000

    Added the imports and dependencies

--- src/autora/theorist/autora_theorist_avengers/__init__.py
+++ src/autora/theorist/autora_theorist_avengers/__init__.py
@@ -3,9 +3,13 @@ Example Theorist
 """
 from typing import Union
 
+from autora.theorist.bms import BMSRegressor
+from autora.experiment_runner.synthetic.psychophysics.weber_fechner_law import weber_fechner_law
 import numpy as np
-import pandas as pd
 from sklearn.base import BaseEstimator
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
